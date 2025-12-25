018eb1306ff52f7a5537b99e49a9245125b3e71c
Chinmay Garde <chinmaygarde@gmail.com>
2025-12-25 11:55:50 -0800

    Even more shit.

--- runtime/bin/process.h
+++ runtime/bin/process.h
@@ -367,7 +367,8 @@ class BufferListBase {
 };
 
 #if defined(DART_HOST_OS_ANDROID) || defined(DART_HOST_OS_FUCHSIA) ||          \
-    defined(DART_HOST_OS_LINUX) || defined(DART_HOST_OS_MACOS)
+    defined(DART_HOST_OS_LINUX) || defined(DART_HOST_OS_MACOS) ||              \
+    defined(DART_HOST_OS_QNX)
 class BufferList : public BufferListBase {
  public:
   BufferList() {}
--- runtime/vm/signal_handler_fuchsia.cc
+++ runtime/vm/signal_handler_fuchsia.cc
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 #include "vm/globals.h"
-#if defined(DART_HOST_OS_FUCHSIA)
+#if defined(DART_HOST_OS_FUCHSIA) || defined(DART_HOST_OS_QNX)
 
 #include "vm/signal_handler.h"
 
